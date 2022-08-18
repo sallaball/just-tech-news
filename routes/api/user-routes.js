68444e84bc0a77a9391fba5a826179f68d8aa6c4
sallaball <sallaball1206@gmail.com>
2022-08-18 17:36:26 +0300

    finished adding hash for password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -38,16 +38,39 @@ router.get('/:id', (req, res) => {
 
 //post /api/users
 router.post('/', (req, res) => {
-    //expects {username: 'lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
     User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
     })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
+      });
+  });
+router.post('/login', (req, res) => {
+    //expects {username: 'lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+          email: req.body.email
+        }
+      }).then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!' });
+          return;
+        }
+    
+        // Verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    
+  
     });
 });
 
@@ -84,7 +107,7 @@ router.delete('/:id', (req, res) => {
     })
     .then(dbUserData => {
         if (!dbUserData) {
-            res.status(400).json({ message: 'No user found with this id'});
+            res.status(404).json({ message: 'No user found with this id' });
             return;
         }
         res.json(dbUserData);
