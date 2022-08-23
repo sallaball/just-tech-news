b617436ece34f6714599a49ff4bd2346f579209e
sallaball <sallaball1206@gmail.com>
2022-08-23 18:31:59 +0300

    lesson 4 done

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,8 +1,7 @@
 async function commentFormHandler(event) {
-    event.preverntDefault();
+    event.preventDefault();
 
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
